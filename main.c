c62d8eabef8133ed05b7ee2f541f8af132607f98
Lukas <lukasbaehr01@gmail.com>
2022-01-17 16:29:46 +0100

    learn learn learn

--- main.c
+++ main.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    printf("Hello world\n");
+    char str [120];
+    int x = 8%2;
 
-    int *alter = 18;
+    printf("%a\n", x);
+
+    if(x == 0){
+        printf("Roman ist cool\n");
+    } else {
+        printf("Robin ist cool\n");
+     }
+
+    printf("F an den Gefallenen Bruder\n");
+
+    printf("Bitte geben sie was ein:");
+    fgets(str, 120, stdin);
+    printf("Eingabe: %s\n", str);
+
+    int alter = 18;
 
     printf("Alter: %d!\n", alter);
 
     if(alter >= 18){
         printf("Du bist Erwachsen!\n");
     } else {
-        printf("Du bist leider Minderjährig!\n");
+        printf("Du bist leider nicht Erwachsen!\n");
     }
 
     system("pause");
